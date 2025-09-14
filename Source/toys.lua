a96536acbb0ac5f089e56d9fae46fdd76b9fcfd6
Falk David <falkdavid@gmx.de>
2025-09-14 23:03:12 +0200

    clamp masses

--- Source/toys.lua
+++ Source/toys.lua
@@ -226,7 +226,8 @@ function Toy:init(instr, world)
   self.joints = {}
   self.sprites = {}
   for _, body_instr in ipairs(instr.bodies) do
-    local mass <const> = (body_instr.dimensions.x * body_instr.dimensions.y) * 0.025
+    local mass = (body_instr.dimensions.x * body_instr.dimensions.y) * 0.025
+    mass = Clamp(mass, 30, 100)
     local body = playbox.body.new(body_instr.dimensions.x, body_instr.dimensions.y, mass)
     body:setCenter(body_instr.position.x, body_instr.position.y)
     body:setRotation(math.rad(body_instr.rotation))
