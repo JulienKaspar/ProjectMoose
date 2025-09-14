18d57c87582238b371f99a89976fda2a6f3b33dc
Falk David <falkdavid@gmx.de>
2025-09-14 21:14:01 +0200

    Cleanup

--- Source/visuals.lua
+++ Source/visuals.lua
@@ -259,7 +259,7 @@ end
 
 function makeRotationImageTable(image)
     local w, h = image:getSize()
-    local sw, sh = w / 4, h / 4
+    local sw, sh = w * IMAGE_SCALING, h * IMAGE_SCALING
     local max_wh = math.floor(math.sqrt(sw * sw + sh * sh)) + 1
     local image_table = gfx.imagetable.new(IMAGE_ROTATION_INCREMENT + 1, max_wh, max_wh)
     local image_index = 0
